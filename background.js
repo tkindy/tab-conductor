4649895d14095fa05560720f7aa4fd1167ea125a
Tyler Kindy <tylerkindy@gmail.com>
2024-07-08 09:24:51 -0400

    Move all highlighted tabs

--- background.js
+++ background.js
@@ -1,11 +1,23 @@
-browser.commands.onCommand.addListener((name, tab) => {
+browser.commands.onCommand.addListener((name, _) => {
   if (name === "move-to-new-window") {
-    moveToNewWindow(tab);
+    moveToNewWindow();
   }
 });
 
-function moveToNewWindow(tab) {
-  browser.windows.create({
-    tabId: tab.id,
+async function moveToNewWindow() {
+  const tabs = await browser.tabs.query({
+    highlighted: true,
+    windowId: browser.windows.WINDOW_ID_CURRENT,
   });
+
+  const window = await browser.windows.create({
+    tabId: tabs[0].id,
+  });
+
+  if (tabs.length > 1) {
+    browser.tabs.move(
+      tabs.slice(1).map((tab) => tab.id),
+      { windowId: window.id, index: -1 }
+    );
+  }
 }
