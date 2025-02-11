c22bf397581a5cc9b68c549a1d14067755ad585c
Tyler Kindy <tylerkindy@gmail.com>
2025-02-10 20:24:45 -0500

    Add 'move to previous window' command

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
 browser.commands.onCommand.addListener((name, _) => {
   if (name === "move-to-new-window") {
     moveToNewWindow();
+  } else if (name === "move-to-previous-window") {
+    moveToPreviousWindow();
   }
 });
 
@@ -20,4 +22,47 @@ async function moveToNewWindow() {
       { windowId: window.id, index: -1 }
     );
   }
+}
+
+const windowStack = [];
+
+browser.windows.onFocusChanged.addListener(async (windowId) => {
+  const window = await browser.windows.get(windowId);
+  if (window.type !== "normal") {
+    return;
+  }
+
+  windowStack.push(windowId);
+  if (windowStack.length > 2) {
+    windowStack.shift();
+  }
+});
+
+async function moveToPreviousWindow() {
+  if (windowStack.length < 2) {
+    console.debug("No previous window to move to");
+    return;
+  }
+
+  const [previousWindowId, currentWindowId] = windowStack;
+  if (previousWindowId === currentWindowId) {
+    console.debug("Previous window is the same as the current window");
+    return;
+  }
+
+  const tabs = await browser.tabs.query({
+    highlighted: true,
+    windowId: currentWindowId,
+  });
+  if (tabs.length === 0) {
+    console.debug("No tabs to move");
+    return;
+  }
+
+  browser.tabs.move(
+    tabs.map((tab) => tab.id),
+    { windowId: previousWindowId, index: -1 }
+  );
+  browser.tabs.update(tabs.at(-1).id, { active: true });
+  browser.windows.update(previousWindowId, { focused: true });
 }
